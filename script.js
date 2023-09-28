a1c6fe59a8ecfe26e59f89349cd6bcfaa0b3a03e
vegapunk24 <146307896+vegapunk24@users.noreply.github.com>
2023-09-28 11:20:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const setSizes = () => {
+  let bookletWidth = window.innerWidth < 1400 ? 0.9 * window.innerWidth : 1400;
+
+  if (window.innerHeight < 450) {
+    bookletWidth = 0.75 * window.innerHeight * 2;
+  }
+
+  document.body.style = `height: ${
+    window.innerHeight
+  }px; --booklet-width: ${bookletWidth}px; --page-width: ${
+    bookletWidth / 3
+  }px; --booklet-height: ${bookletWidth / 2}px`;
+};
+
+setSizes();
+
+window.addEventListener("resize", () => {
+  setSizes();
+});
+
+const booklet = document.querySelector(".booklet");
+const button = document.querySelector(".openBtn");
+
+button.addEventListener("click", () => {
+  button.classList.add("hide");
+  setTimeout(() => {
+    button.classList.remove("hide");
+  }, 2000);
+  if (booklet.classList.contains("open")) {
+    booklet.classList.remove("open");
+  } else {
+    booklet.classList.add("open");
+  }
+});
